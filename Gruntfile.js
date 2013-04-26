8f8a8fe5a59dc68534305ed5e1b649a8b05c7d1d
Ben Paddock <pads@thisispads.me.uk>
2013-04-26 17:41:42 +0100

    Include jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,34 @@ module.exports = function(grunt) {
                 command: "cd app && tsapp push tsfeed_public",
                 stdout: true
             }
+        },
+        jshint: {
+            all: ["Gruntfile.js", "app/assets/*.js"],
+            options: {
+                "bitwise": true,
+                "camelcase": true,
+                "curly": true,
+                "eqeqeq": true,
+                "forin": true,
+                "immed": true,
+                "indent": 4,
+                "latedef": true,
+                "newcap": true,
+                "noarg": true,
+                "noempty": true,
+                "quotmark": "double",
+                "trailing": true,
+                "maxlen": 120,
+                "undef": true,
+                "unused": true,
+                "boss": true,
+                "browser": true,
+                "sub": true,
+                "globals": {
+                    "google": true,
+                    "alert": true
+                }
+            }
         }
     });
 
@@ -35,7 +63,9 @@ module.exports = function(grunt) {
         grunt.task.run("exec:tsserve");
     });
 
+    grunt.registerTask("default", ["jshint"]);
+
     grunt.loadNpmTasks("grunt-exec");
     grunt.loadNpmTasks("grunt-contrib-copy");
-
+    grunt.loadNpmTasks("grunt-contrib-jshint");
 };
